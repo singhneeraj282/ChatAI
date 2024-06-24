85017bd32cffdabe55b6cbcaac6a93cfb8d88b94
singhneeraj282 <singhneeraj.282@gmail.com>
2024-06-25 00:39:44 +0530

    Setup exception handler for failed call to openAI

---
+++ src/main/java/com/tera/ChatAI/config/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.tera.ChatAI.config;
+
+import org.springframework.ai.retry.NonTransientAiException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.HttpRetryException;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {HttpRetryException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "You exceeded your current quota, please check your plan and billing details. For more information on this error, read the docs: https://platform.openai.com/docs/guides/error-codes/api-errors.";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.PAYMENT_REQUIRED, request);
+    }
+}
--- src/main/java/com/tera/ChatAI/config/SwaggerConfig.java
+++
@@ -1,6 +0,0 @@
-package com.tera.ChatAI.config;
-
-/*@EnableSwagger2WebMvc
-@Import(SpringDataRestConfiguration.class)*/
-public class SwaggerConfig {
-}
